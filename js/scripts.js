f8b504f370438f3dc1baa3e71072315e7a4aec49
amare1990 <amaremek@gmail.com>
2022-09-13 18:58:53 +0300

    Modify the css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-const humburger = document.querySelector('.navbar');
-wrapperMain = document.querySelector('.wrapper');
+const hamburger = document.querySelector('.navbar');
+const wrapperMain = document.querySelector('.wrapper');
+const mobileMenuContainer = document.querySelector('.mobile-menu-wrapper');
+const mobileMenuItem = document.querySelectorAll('.mobile-menu-item');
+const closeButton = document.querySelector('.closeBtn');
+const icon = document.querySelector('.fa-bars');
+
+//mobileMenuContainer.style.display = 'none';
 
 function mobileMenuDisplay(){
-  wrapperMain.style.display = 'none';
 
+  if(icon.classList.contains('fa-bars')){
+    icon.classList.replace('fa-bars', 'fa-times');
+    mobileMenuContainer.style.display = 'block';
+  }
+  else {
+      icon.classList.replace('fa-times', 'fa-bars');
+      mobileMenuContainer.style.display = 'none';
+  }
+
+  //wrapperMain.style.display = 'none';
+  
 }
 
-humburger.addEventListener('click', mobileMenuDisplay);
+hamburger.addEventListener('click', mobileMenuDisplay);
+closeButton.addEventListener('click', mobileMenuDisplay);
