77b9d47cea8d9bd52e63b0e8ecff59243025cd68
amare1990 <amaremek@gmail.com>
2022-09-14 20:00:41 +0300

    Remove blurred homepage when menu options are clicked

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ const mobileMenuItem = document.querySelectorAll('.mobile-menu-item');
 const closeButton = document.querySelector('.closeBtn');
 const icon = document.querySelector('.fa-bars');
 const iconTimes = document.querySelector('.fa-times');
-const homepage = document.querySelector('.container-homepage');
+const headerAndHomepage = document.querySelector('.header-and-homepage-container');
+const deviceStatusNormal = document.querySelector('.status-bar');
 
 //mobileMenuContainer.style.display = 'none';
 
@@ -14,12 +15,20 @@ function mobileMenuDisplay(){
 
   if(icon.classList.contains('fa-bars')){
     icon.classList.replace('fa-bars', 'fa-times');
+    //deviceStatusNormal.style.display = 'none';
     mobileMenuContainer.style.display = 'block';
-    homepage.style.filter = 'blur(2px)';
+    //mobileMenuContainer.style.marginTop = '0';
+    headerAndHomepage.style.filter = 'blur(7px)';
+    //deviceStatus.classList.add('brightness');
+    //deviceStatusNormal.toggle('hamburger-clicked');
+    
+    //headerAndHomepage.style.backgroundColor = red;
   }
   else {
       mobileMenuContainer.style.display = 'none';
       icon.classList.replace('fa-times', 'fa-bars');
+      headerAndHomepage.style.filter = '1';
+      //headerAndHomepage.style.filter = '100%';
   }
   //wrapperMain.style.display = 'none';
   
@@ -42,6 +51,7 @@ document.querySelector('.mobile-menu-close').addEventListener('click',() =>{
 
 document.querySelector('.portfolio_mobile').addEventListener('click',() =>{
 const menu = document.querySelector('.mobile-menu-wrapper');
+headerAndHomepage.style.filter = 'none';
 menu.style.display = 'none';
 window.scroll({
   top: 875,
@@ -51,6 +61,7 @@ window.scroll({
 document.querySelector('.about_mobile').addEventListener('click',() =>{
   const menu = document.querySelector('.mobile-menu-wrapper');
   menu.style.display = 'none';
+  headerAndHomepage.style.filter = 'none';
   window.scroll({
     top: 4300,
     behavior: 'smooth'
@@ -60,10 +71,9 @@ document.querySelector('.about_mobile').addEventListener('click',() =>{
 document.querySelector('.contact_mobile').addEventListener('click',() =>{
     const menu = document.querySelector('.mobile-menu-wrapper');
     menu.style.display = 'none';
+    headerAndHomepage.style.filter = 'none';
     window.scroll({
       top: 5450,
       behavior: 'smooth'
       });
-    });
-
- 
+});
