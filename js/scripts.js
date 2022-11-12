cb6ec210445f0c1ccbeed138c17dfd7aac836009
amare1990 <amaremek@gmail.com>
2022-11-12 17:06:57 +0300

    Update CSS styles

--- js/scripts.js
+++ js/scripts.js
@@ -85,42 +85,42 @@ document.querySelector('.contact_mobile').addEventListener('click',() =>{
 // Values initially for mobile version
 const projects = [ 
   {
-      title: 'ICTD4A',
+      title: 'Awesome Books',
       counterImge: './images/Counter.png',
-      experiences: ['Canopy', 'Back End Dev', 'Counter', '2015'],
-      featuredImage: './images/ictd4a-images/mob-ictd4.png',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+      experiences: ['Microverse Inc', 'Front End Dev', '2022'],
+      featuredImage: './images/ictd4a-images/awesomebook-mob.png',
+      description: "This project is a web application project that allows the client to add a book and dynamically display all the added books.",
       technologies: ['', 'html', 'css', 'javascript'],
       linkToLiveVersion: '',
-      linkToSource: 'https://github.com/amare1990/ICT4DA'
+      linkToSource: 'https://github.com/amare1990/AwesomeBooks'
     },
     {
-    title: 'Awesome Books',
+    title: 'Math Magicians',
     counterImge: './images/Counter.png',
-    experiences: ['Canopy', 'Back End Dev', '2015'],
-    featuredImage: './images/ictd4a-images/awesome-books.png',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    experiences: ['Microverse Inc', 'Front End Dev', '2022'],
+    featuredImage: './images/ictd4a-images/math-magician-mob.png',
+    description: "This project is a claculator web application created by React JavaScript library.  It allowers users to calculate basic mathematics. The objective of this project is to practice how to create react components, use react props and states.",
     technologies: ['', 'html', 'css', 'javascript'],
     linkToLiveVersion: '',
-    linkToSource: 'https://github.com/amare1990/AwesomeBooks'
+    linkToSource: 'https://github.com/amare1990/math-magicians'
     },
     {
-    title: 'Tonic 2',
-    experiences: ['Canopy', 'Back End Dev', '2015'],
-    featuredImage: './images/ictd4a-images/awesome-books-add.png',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    title: 'Todos List',
+    experiences: ['Microverse Inc', 'Front End Dev', '2022'],
+    featuredImage: './images/ictd4a-images/todos.png',  
+    description: "Developed with HTML, CSS and JavaScript, this web application allows customers to add tasks to be completed today, edit the tasks, and interactively delete completed tasks both on the screen and in the localStorage object.",
     technologies: ['', 'html', 'css', 'javascript'],
     linkToLiveVersion: '',
-    linkToSource: 'https://github.com/amare1990/AwesomeBooks'
+    linkToSource: 'https://github.com/amare1990/To-do-List'
     },
     {
-      title: 'Multi-Post Stories',
-      experiences: ['Canopy', 'Back End Dev', '2015'],
-      featuredImage: './images/ictd4a-images/awesome-books-add.png',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+      title: 'LeaderBoard',
+      experiences: ['Microverse Inc', 'Front End Dev', '2022'],
+      featuredImage: './images/ictd4a-images/leaderboard.png',
+      description: "LeaderBoard is a web application project that allows people to submit score of the player and see the score on the screen. It stores and fetches score result fom the remore API. HTML, CSS, and mainly JavaScript is used to implement this project.",
       technologies: ['', 'html', 'css', 'javascript'],
       linkToLiveVersion: '',
-      linkToSource:'https://github.com/amare1990/AwesomeBooks'
+      linkToSource:'https://github.com/amare1990/LeaderBoard'
     }
 ]
 
@@ -252,29 +252,42 @@ function mobileVersion() {
 function desktopVersion() {  
   
    projects[0].title = 'ICTD4A';
-   projects[1].title = 'Multi-Post Stories';
-   projects[2].title = 'Facebook 360';
-   projects[3].title = 'Uber Navigation';
-   projects[0].experiences = ['Canopy', 'Back End Dev', '2015'];
-   projects[1].experiences = ['Facebook', 'Full Stack Dev', '2015'];
-   projects[2].experiences = ['Facebook 360', 'Full Stack Dev', '2015'];
-   projects[3].experiences = ['Uber Navigation', 'Lead Developer', '2018'];
+   projects[1].title = 'JavaScript Capstone Project';
+   projects[2].title = 'Todos List';
+   projects[3].title = 'LeaderBoard';
+   
+   projects[0].experiences = ['Microverse Inc', 'Front-end Dev', '2022'];
+   projects[1].experiences = ['Microverse Inc', 'Front-end Dev Dev', '2022'];
+   projects[2].experiences = ['Microverse Inc', 'Front-end Dev', '2022'];
+   projects[3].experiences = ['Microverse Inc', 'Front-end Dev', '2022'];
   
-   projects[0].technologies = ['', 'html', 'CSS', 'javascript'];
-   projects[1].technologies = ['', 'html', 'Ruby on rails' ,'css', 'javascript'];
-   projects[2].technologies = ['', 'html', 'Ruby on rails' ,'css', 'javascript'];
-   projects[3].technologies = ['', 'html', 'Ruby on rails' ,'css', 'javascript'];
+   projects[0].technologies = ['', 'html', 'css', 'javascript'];
+   projects[1].technologies = ['', 'html','css', 'javascript'];
+   projects[2].technologies = ['', 'html','css', 'javascript'];
+   projects[3].technologies = ['', 'html','css', 'javascript'];
   
-   projects[0].featuredImage = './images/ictd4a-images/awesome-books.png';
-   projects[1].featuredImage = './images/ictd4a-images/desk-ictd4a.png';
-   projects[2].featuredImage = './images/ictd4a-images/desk-speakers.png';
-   projects[3].featuredImage = './images/ictd4a-images/desk-partners.png';
+   projects[0].featuredImage = './images/ictd4a-images/ictd4a-desk.png';
+   projects[1].featuredImage = './images/ictd4a-images/canadian-food.png';
+   projects[2].featuredImage = './images/ictd4a-images/todos.png';
+   projects[3].featuredImage = './images/ictd4a-images/leaderboard.png';
   
-   projects[0].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
-   projects[1].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
-   projects[2].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
-   projects[3].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
+   projects[0].description = "This is a web application project for the Research Conference Call for Paper. It is developed as a Microverse Capstone project for Module 1. It is developed using HTML, CSS, and JavaSript. It is a two page web application that is interactive and dynamic as it is developed using JavaScript.";
+   projects[1].description = "This project is a JavaScript capstone project from Microverse for Module 2. As a capstone project, it requires the previous advanced four weeks JavaScript intensive learning and some web development background. this project is very dynamic web application that fetches meal from the Canadian meal API. It allows customers to like and comment the meal ";
+   projects[2].description = "Developed with HTML, CSS and JavaScript, this web application allows customers to add tasks to be completed today, edit the tasks, and interactively delete completed tasks both on the screen and in the localStorage object.";
+   projects[3].description = "LeaderBoard is a web application project that allows people to submit score of the player and see the score on the screen. It stores and fetches score result fom the remore API. HTML, CSS, and mainly JavaScript is used to implement this project.";
   
+   projects[0].linkToLiveVersion = 'https://amare1990.github.io/ICT4DA/';
+   projects[0].linkToSource = 'https://github.com/amare1990/ICT4DA';
+
+   projects[1].linkToLiveVersion = 'https://www.loom.com/share/2e5639ffe4904400ad7a9baaba57e9f7';
+   projects[1].linkToSource = 'https://github.com/amare1990/JavaScript-Capstone-Project';
+
+   projects[2].linkToLiveVersion = '';
+   projects[2].linkToSource = 'https://github.com/amare1990/To-do-List';
+
+   projects[3].linkToLiveVersion = '';
+   projects[3].linkToSource = 'https://github.com/amare1990/LeaderBoard';
+
   for(let i = 0; i < projects.length; i++) {
     const gridsContainer = document.querySelector('.grids-wrapper');
     let gridDiv = document.createElement('div');
@@ -320,8 +333,6 @@ function desktopVersion() {
   
   
   
-  
-  
             <div class = "grid-body grid-body${i + 1}">
               <h2 class="grid-title">${projects[i].title}</h2>
               <ul class="experience-container">
@@ -356,7 +367,7 @@ function desktopVersion() {
   // Popup window starts here
   projects[0].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.";
   projects[1].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.";
-  projects[2].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.";
+  projects[2].description = "Developed with HTML, CSS and JavaScript, this web application allows customers to add tasks to be completed today, edit the tasks, and interactively delete completed tasks both on the screen and in the localStorage object.";
   projects[3].description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.";
   
   const popupWindow = document.querySelector('.popup-card-wrapper');
@@ -403,11 +414,12 @@ function desktopVersion() {
               </ul>
               <div class="popup-button-group">
                 <div class="popup-knowBtns">
-                  <button type="button" class="popup-btn-text">See live</button>
+                  <button type="button" class="popup-btn-text">
+                  <a href = "${projects[index].linkToLiveVersion}"> See live  </a> </button>
                   <img src="./images/popup/Icon-Export-btn1.png" alt="">
                 </div>
                 <div class="popup-knowBtns">
-                  <button type="button" class="popup-btn-text">   <a href = "https://github.com/amare1990/ICT4DA">See source </a> </button>
+                  <button type="button" class="popup-btn-text">   <a href = "${projects[index].linkToSource}">See source </a> </button>
                 
                   <img src="./images/popup/Icon-GitHub-popbt2.png " alt="">
                 </div>
